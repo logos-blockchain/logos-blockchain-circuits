b71a648643983eb933c0c46da741bb46b9cec57e
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-01-28 16:38:11 +0100

    Add separate versions file.

---
+++ versions.nix
@@ -0,0 +1,13 @@
+{
+  "0.3.2" = {
+    x86_64-linux = "sha256-80+GrB3kBhwLHvNemme5Vig6tPDRRZC7xHps0DNonzM=";
+    aarch64-darwin = "sha256-FbLgrHaa8djFEaA69WpZMB3uozkLT/abQiCWKrkzcsk=";
+    x86_64-windows = "sha256-VOBUXlXNHTY0l91G+B1vybDfES0Y0HXhUytJIfFEiBA=";
+  };
+  "0.4.0" = {
+    x86_64-linux   = "sha256-Oi3xhqm5Sd4PaCSHWMvsJm2YPtSlm11BBG99xG30tiM=";
+    aarch64-linux  = "sha256-8lsgqflHXPP6mnxILpUCNhetpVeDNOXiQlWKoZLHa7I=";
+    aarch64-darwin = "sha256-E+yMjJPMy08jbiHLlDmDvlKnGJ4UiIRKB9GGZ0JGBB8=";
+    x86_64-windows = "sha256-8qceJxNt+OGF5cRNwNG146Op5xcqbShQEtmVJ6iDvmQ=";
+  };
+}
