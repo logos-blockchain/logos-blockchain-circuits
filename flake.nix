d8992faa1d2d8b2ddfc45cd01b93999ebc8b79b1
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-01-28 16:26:46 +0100

    Format flake.

--- flake.nix
+++ flake.nix
@@ -5,38 +5,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs =
+    { nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
 
-      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-windows" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-windows"
+      ];
       forAll = lib.genAttrs systems;
 
       circuitsVersion = "0.3.2";
 
       circuitsHashes = {
-        x86_64-linux   = "sha256-80+GrB3kBhwLHvNemme5Vig6tPDRRZC7xHps0DNonzM=";
+        x86_64-linux = "sha256-80+GrB3kBhwLHvNemme5Vig6tPDRRZC7xHps0DNonzM=";
         aarch64-darwin = "sha256-FbLgrHaa8djFEaA69WpZMB3uozkLT/abQiCWKrkzcsk=";
         x86_64-windows = "sha256-VOBUXlXNHTY0l91G+B1vybDfES0Y0HXhUytJIfFEiBA=";
       };
 
-      githubBase =
-        "https://github.com/logos-blockchain/logos-blockchain-circuits/releases/download";
+      githubBase = "https://github.com/logos-blockchain/logos-blockchain-circuits/releases/download";
 
-      mkCircuits = system:
+      mkCircuits =
+        system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
 
           os =
-            if pkgs.stdenv.isLinux then "linux"
-            else if pkgs.stdenv.isDarwin then "macos"
-            else if pkgs.stdenv.isWindows then "windows"
-            else throw "Unsupported OS";
+            if pkgs.stdenv.isLinux then
+              "linux"
+            else if pkgs.stdenv.isDarwin then
+              "macos"
+            else if pkgs.stdenv.isWindows then
+              "windows"
+            else
+              throw "Unsupported OS";
 
           arch =
-            if pkgs.stdenv.isx86_64 then "x86_64"
-            else if pkgs.stdenv.isAarch64 then "aarch64"
-            else throw "Unsupported architecture.";
+            if pkgs.stdenv.isx86_64 then
+              "x86_64"
+            else if pkgs.stdenv.isAarch64 then
+              "aarch64"
+            else
+              throw "Unsupported architecture.";
         in
         pkgs.stdenv.mkDerivation {
           pname = "logos-blockchain-circuits";
@@ -61,7 +73,8 @@
         };
     in
     {
-      packages = forAll (system:
+      packages = forAll (
+        system:
         let
           circuits = mkCircuits system;
         in
