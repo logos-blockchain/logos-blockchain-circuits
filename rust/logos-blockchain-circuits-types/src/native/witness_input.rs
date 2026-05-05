c7b5f7ac9171c4d5c9d017b5ea999694b7281c95
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-05-05 16:50:55 +0200

    Improve lifetime name.

--- rust/logos-blockchain-circuits-types/src/native/witness_input.rs
+++ rust/logos-blockchain-circuits-types/src/native/witness_input.rs
@@ -3,15 +3,15 @@ use crate::ffi;
 use crate::native::Error;
 
 /// Input for witness generators
-pub struct WitnessInput<'a> {
+pub struct WitnessInput<'dat> {
     /// The circuit's dat file contents.
-    dat: &'a [u8],
+    dat: &'dat [u8],
     /// The JSON string containing the circuit inputs.
     inputs_json: CString,
 }
 
-impl<'a> WitnessInput<'a> {
-    pub fn new(dat: &'a [u8], inputs_json: String) -> Result<Self, Error> {
+impl<'dat> WitnessInput<'dat> {
+    pub fn new(dat: &'dat [u8], inputs_json: String) -> Result<Self, Error> {
         let inputs_json = CString::new(inputs_json).map_err(
             |error| Error::InvalidInput(Some(
                 format!("The parameter inputs_json could not be converted to CString: {}", error)
@@ -27,13 +27,13 @@ impl<'a> WitnessInput<'a> {
 }
 
 /// Temporary FFI view of a [`WitnessInput`], which makes [`ffi::WitnessInput`] lifetime-aware.
-pub struct WitnessInputFfiGuard<'a> {
+pub struct WitnessInputFfiGuard<'dat> {
     ffi: ffi::WitnessInput,
-    _lifetime: std::marker::PhantomData<&'a WitnessInput<'a>>,
+    _lifetime: std::marker::PhantomData<&'dat WitnessInput<'dat>>,
 }
 
-impl<'a> WitnessInputFfiGuard<'a> {
-    fn new(inner: &'a WitnessInput) -> Self {
+impl<'dat> WitnessInputFfiGuard<'dat> {
+    fn new(inner: &'dat WitnessInput) -> Self {
         let dat = ffi::ConstBytes { data: inner.dat.as_ptr(), size: inner.dat.len() };
         let inputs_json = inner.inputs_json.as_ptr();
         let ffi = ffi::WitnessInput { dat, inputs_json };
@@ -44,7 +44,7 @@ impl<'a> WitnessInputFfiGuard<'a> {
     }
 }
 
-impl<'a> AsRef<ffi::WitnessInput> for WitnessInputFfiGuard<'a> {
+impl<'dat> AsRef<ffi::WitnessInput> for WitnessInputFfiGuard<'dat> {
     fn as_ref(&self) -> &ffi::WitnessInput {
         &self.ffi
     }
